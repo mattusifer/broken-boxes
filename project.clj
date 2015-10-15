e4f3e52fa4c75100cc6b9fa6e8f8c565dfbd5964
Matt Usifer <mattusifer@gmail.com>
2015-10-15 14:19:50 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [thi.ng/geom "0.0.881"]
-                 [quil "2.2.6"]
-                 [net.cgrand/utils "0.1.0-SNAPSHOT"]]
+                 [quil "2.2.6"]]
   :main ^:skip-aot broken-boxes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
